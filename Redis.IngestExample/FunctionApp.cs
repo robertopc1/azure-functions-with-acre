2f7ec8673a0997a3c5e0019e3b6ea3e127c1145e
roberto.perez <robertopc@gmail.com>
2024-05-08 14:19:26 -0400

    Latest updates

--- Redis.IngestExample/FunctionApp.cs
+++ Redis.IngestExample/FunctionApp.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Redis;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Redis.OM.Contracts;
 using Redis.OM.Searching;
@@ -17,16 +18,18 @@ public class FunctionApp
     private readonly IRedisCollection<Styles> _stylesCollection;
     private readonly IRedisConnectionProvider _connectionProvider;
     private const string RedisConnectionSetting = "RedisConnectionString";
-
-    public FunctionApp(ILoggerFactory loggerFactory, 
-           IRedisConnectionProvider connectionProvider
-            )
+    private const string SubscriptionChannel = "__keyevent@0__:set";
+    public FunctionApp(ILoggerFactory loggerFactory
+           //IRedisConnectionProvider connectionProvider
+    )
     {
         _logger = loggerFactory.CreateLogger<FunctionApp>();
-        _connectionProvider = connectionProvider ?? throw new ArgumentNullException();
-        _stylesCollection = _connectionProvider.RedisCollection<Styles>();
+        // _connectionProvider = connectionProvider ?? throw new ArgumentNullException();
+        // _stylesCollection = _connectionProvider.RedisCollection<Styles>();
     }
-
+    
+    public string SQLAddress = System.Environment.GetEnvironmentVariable("SQLConnectionString");
+    
     // Visit https://aka.ms/sqltrigger to learn how to use this trigger binding
     // [Function("IngestWithOmExample")]
     // public async Task Run(
@@ -60,63 +63,63 @@ public class FunctionApp
     //**** Not Working ** //
     [Function(nameof(WriteBehind))]
     public void WriteBehind(
-    [RedisPubSubTrigger(RedisConnectionSetting, "__keyevent@0__:set")] string newKey,
-    [RedisInput("RedisConnectionString", "GET {Message}")] string setValue)
+    [RedisPubSubTrigger(RedisConnectionSetting, SubscriptionChannel)] string message,
+    [RedisInput(RedisConnectionSetting, "GET {Message}")] string setValue)
     {
-        _logger.LogInformation($"My Value:{newKey}");
+        _logger.LogInformation($"My Value:{message}");
         
-        // //var key = channelMessage.Message; //The name of the key that was set
-        // //var value = 0.0;
-        //
-        // //Check if the value is a number. If not, log an error and return.
-        // //if (double.TryParse(setValue, out double result))
-        // {
-        //     value = result; //The value that was set. (i.e. the price.)
-        //     logger.LogInformation($"Key '{channelMessage.Message}' was set to value '{value}'");
-        // }
-        // else
-        // {
-        //     logger.LogInformation($"Invalid input for key '{key}'. A number is expected.");
-        //     return;
-        // }        
-        //
-        // // Define the name of the table you created and the column names.
-        // String tableName = "dbo.inventory";
-        // String column1Value = "ItemName";
-        // String column2Value = "Price";        
-        //
-        // logger.LogInformation($" '{SQLAddress}'");
-        // using (SqlConnection connection = new SqlConnection(SQLAddress))
-        //     {
-        //         connection.Open();
-        //         using (SqlCommand command = new SqlCommand())
-        //         {
-        //             command.Connection = connection;
-        //
-        //             //Form the SQL query to update the database. In practice, you would want to use a parameterized query to prevent SQL injection attacks.
-        //             //An example query would be something like "UPDATE dbo.inventory SET Price = 1.75 WHERE ItemName = 'Apple'".
-        //             command.CommandText = "UPDATE " + tableName + " SET " + column2Value + " = " + value + " WHERE " + column1Value + " = '" + key + "'";
-        //             int rowsAffected = command.ExecuteNonQuery(); //The query execution returns the number of rows affected by the query. If the key doesn't exist, it will return 0.
-        //
-        //             if (rowsAffected == 0) //If key doesn't exist, add it to the database
-        //          {
-        //                  //Form the SQL query to update the database. In practice, you would want to use a parameterized query to prevent SQL injection attacks.
-        //                  //An example query would be something like "INSERT INTO dbo.inventory (ItemName, Price) VALUES ('Bread', '2.55')".
-        //                 command.CommandText = "INSERT INTO " + tableName + " (" + column1Value + ", " + column2Value + ") VALUES ('" + key + "', '" + value + "')";
-        //                 command.ExecuteNonQuery();
-        //
-        //                 logger.LogInformation($"Item " + key + " has been added to the database with price " + value + "");
-        //             }
-        //
-        //             else {
-        //                 logger.LogInformation($"Item " + key + " has been updated to price " + value + "");
-        //             }
-        //         }
-        //         connection.Close();
-        //     }
-        //
-        //     //Log the time that the function was executed.
-        //     logger.LogInformation($"C# Redis trigger function executed at: {DateTime.Now}");
+         var key = message; //The name of the key that was set
+         var value = 0.0;
+        
+        //Check if the value is a number. If not, log an error and return.
+        if (double.TryParse(setValue, out double result))
+        {
+            value = result; //The value that was set. (i.e. the price.)
+            _logger.LogInformation($"Key '{message}' was set to value '{value}'");
+        }
+        else
+        {
+            _logger.LogInformation($"Invalid input for key '{key}'. A number is expected.");
+            return;
+        }        
+        
+        // Define the name of the table you created and the column names.
+        String tableName = "dbo.inventory";
+        String column1Value = "ItemName";
+        String column2Value = "Price";        
+        
+        _logger.LogInformation($" '{SQLAddress}'");
+        using (SqlConnection connection = new SqlConnection(SQLAddress))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+        
+                //Form the SQL query to update the database. In practice, you would want to use a parameterized query to prevent SQL injection attacks.
+                //An example query would be something like "UPDATE dbo.inventory SET Price = 1.75 WHERE ItemName = 'Apple'".
+                command.CommandText = "UPDATE " + tableName + " SET " + column2Value + " = " + value + " WHERE " + column1Value + " = '" + key + "'";
+                int rowsAffected = command.ExecuteNonQuery(); //The query execution returns the number of rows affected by the query. If the key doesn't exist, it will return 0.
+        
+                if (rowsAffected == 0) //If key doesn't exist, add it to the database
+                {
+                     //Form the SQL query to update the database. In practice, you would want to use a parameterized query to prevent SQL injection attacks.
+                     //An example query would be something like "INSERT INTO dbo.inventory (ItemName, Price) VALUES ('Bread', '2.55')".
+                    command.CommandText = "INSERT INTO " + tableName + " (" + column1Value + ", " + column2Value + ") VALUES ('" + key + "', '" + value + "')";
+                    command.ExecuteNonQuery();
+        
+                    _logger.LogInformation($"Item " + key + " has been added to the database with price " + value + "");
+                }
+        
+                else {
+                    _logger.LogInformation($"Item " + key + " has been updated to price " + value + "");
+                }
+            }
+            connection.Close();
+        }
+        
+        //Log the time that the function was executed.
+        _logger.LogInformation($"C# Redis trigger function executed at: {DateTime.Now}");
     }
 }
 
--- Redis.IngestExample/Program.cs
+++ Redis.IngestExample/Program.cs
@@ -9,23 +9,23 @@ using StackExchange.Redis;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices((hostContext, services) =>
-    {
-        IConfiguration configuration = hostContext.Configuration;
-        string redisConnectionString = configuration["RedisConnectionString"];
+    // .ConfigureServices((hostContext, services) =>
+    // {
+        // IConfiguration configuration = hostContext.Configuration;
+        // string redisConnectionString = configuration["RedisConnectionString"];
         
-        var redisMultiplexer = ConnectionMultiplexer.Connect($"{redisConnectionString}"); // Replace with your Redis server connection details
-        var provider = new RedisConnectionProvider(redisMultiplexer);
-        var connection = provider.Connection;
+        // var redisMultiplexer = ConnectionMultiplexer.Connect($"{redisConnectionString}"); // Replace with your Redis server connection details
+        // var provider = new RedisConnectionProvider(redisMultiplexer);
+        // var connection = provider.Connection;
         
-        var stylesCollection = provider.RedisCollection<Styles>();
+        // var stylesCollection = provider.RedisCollection<Styles>();
 
-        // Create index
-        connection.CreateIndex(typeof(Styles));
+        // // Create index
+        // connection.CreateIndex(typeof(Styles));
 
-        // Add services to the container.
-        services.AddSingleton<IRedisConnectionProvider>(provider);
-    })
+        // // Add services to the container.
+        // services.AddSingleton<IRedisConnectionProvider>(provider);
+    // })
     .Build();
 
 host.Run();
