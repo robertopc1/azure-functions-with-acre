d639edd07e363fe2a67980c6a3b415fac77863d9
roberto.perez <robertopc@gmail.com>
2024-05-11 17:31:44 -0400

    Updates

--- Redis.IngestExample/FunctionApp.cs
+++ Redis.IngestExample/FunctionApp.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Redis;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
 using Redis.OM.Contracts;
 using Redis.OM.Searching;
@@ -24,23 +27,40 @@ public class FunctionApp
         _data = stylesData ?? throw new ArgumentNullException(nameof(stylesData));
     }
     
-    public string SQLAddress = System.Environment.GetEnvironmentVariable("SQLConnectionString");
-    
     // Visit https://aka.ms/sqltrigger to learn how to use this trigger binding
-    // [Function("IngestWithOmExample")]
-    // public async Task Run(
-    //     [SqlTrigger("[aidemo].[styles]", "SQLConnectionString")] 
-    //     IReadOnlyList<SqlChange<Styles>> changes) 
+    [Function("IngestWithOmExample")]
+    public async Task Run(
+        [SqlTrigger("[aidemo].[styles]", "SQLConnectionString")] 
+        IReadOnlyList<SqlChange<Styles>> changes) 
+    {
+        IEnumerable<Styles> changeList = changes.Select<SqlChange<Styles>, Styles>(change => change.Item);
+        await _stylesCollection.UpdateAsync(changeList);
+    }
+    
+    // The RedisInput binding only supports single output commands at the moment
+    //https://github.com/Azure/azure-functions-redis-extension/blob/main/src/Microsoft.Azure.WebJobs.Extensions.Redis/Bindings/RedisAsyncConverter.cs#L63
+    // since in this example we are working with json documents, we will use OM at the moment. 
+    // [Function(nameof(WriteBehind))]
+    // public async Task WriteBehind(
+    // [RedisPubSubTrigger(Common.ConnectionString, Common.SubscriptionChannel)] Common.ChannelMessage channelMessage)
     // {
-    //     IEnumerable<Styles> changeList = changes.Select<SqlChange<Styles>, Styles>(change => change.Item);
-    //     await _stylesCollection.UpdateAsync(changeList);
+    //     var key = channelMessage.Message; //The name of the key that was set
+    //     var value = await _stylesCollection.FindByIdAsync(key);
+    //     
+    //     if(value != null)
+    //         // Update the row in SQL
+    //         await _data.UpdateStyle(value);
+    //     
+    //     //Log the time that the function was executed.
+    //     _logger.LogInformation($"C# Redis trigger function executed at: {DateTime.Now}");
     // }
     
-    //**** Not Working ** //
+    
+    //*** WIP ****//
     // [Function("IngestWithInputBiding")]
     // [RedisOutput("RedisConnectionString", "JSON.MSET")]
     // public string Run(
-    //     [SqlTrigger("[aidemo].[styles]", "ConnectionString")]
+    //     [SqlTrigger("[aidemo].[styles]", "SQLConnectionString")]
     //     IReadOnlyList<SqlChange<Styles>> changes)
     // {
     //     IEnumerable<Styles> changeList = changes.Select<SqlChange<Styles>, Styles>(change => change.Item);
@@ -50,28 +70,12 @@ public class FunctionApp
     //     foreach (var item in changeList)
     //     {
     //         var jsonString = JsonSerializer.Serialize(item);
-    //         jsonStringCollection.Append($"Redis.IngestExample.Styles:{item.id} $ {item} ");
+    //         jsonStringCollection.Append($"Redis.IngestExample.Styles:{item.id} $ {jsonString}");
     //     }
     //     
-    //     return $"${jsonStringCollection}";
+    //     return $"{jsonStringCollection}";
     // }
     
-    // The RedisInput binding only supports single output commands at the moment
-    //https://github.com/Azure/azure-functions-redis-extension/blob/main/src/Microsoft.Azure.WebJobs.Extensions.Redis/Bindings/RedisAsyncConverter.cs#L63
-    // since in this example we are working with json documents, we will use OM at the moment. 
-    [Function(nameof(WriteBehind))]
-    public async Task WriteBehind(
-    [RedisPubSubTrigger(Common.ConnectionString, Common.SubscriptionChannel)] Common.ChannelMessage channelMessage)
-    {
-        var key = channelMessage.Message; //The name of the key that was set
-        var value = await _stylesCollection.FindByIdAsync(key);
-        
-        if(value != null)
-            // Update the row in SQL
-            await _data.UpdateStyle(value);
-        
-        //Log the time that the function was executed.
-        _logger.LogInformation($"C# Redis trigger function executed at: {DateTime.Now}");
-    }
+    
 }
 
